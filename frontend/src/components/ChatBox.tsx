49d29bb2500e20707ff11bad7b852e843abb5354
Gabriele Venturi <lele.venturi@gmail.com>
2024-08-31 00:32:49 +0200

    style: improve chat UI

--- frontend/src/components/ChatBox.tsx
+++ frontend/src/components/ChatBox.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
+import Image from "next/image";
 import { Button } from "./ui/Button";
 import { Input } from "./ui/Input";
 import { chat, chatStatus } from "@/services/chat";
@@ -8,11 +9,18 @@ import { useQuery } from "@tanstack/react-query";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
+import { motion } from "framer-motion";
 
-export const NoChatPlaceHolder = () => {
+export const NoChatPlaceholder = () => {
   return (
-    <div className="flex-1 flex flex-col items-center justify-center overflow-y-auto p-4 bg-white shadow-md rounded-lg">
-      <img src="/build_chat.svg" className="w-96 h-96 mb-6" />
+    <div className="flex-1 flex flex-col items-center justify-center">
+      <Image
+        src="/build_chat.svg"
+        className="mb-6"
+        alt="Chat loaing"
+        width={384}
+        height={384}
+      />
       <div className="text-center max-w-lg">
         Please wait a moment while we process the uploaded documents for chat.
         This may take a few minutes. Feel free to revisit this page shortly.
@@ -24,6 +32,7 @@ export const NoChatPlaceHolder = () => {
 interface ChatMessage {
   sender: string;
   text: string;
+  timestamp: Date;
 }
 
 interface ChatProps {
@@ -61,7 +70,7 @@ const ChatBox = ({
     if (input.trim() === "") return;
     if (!project_id) return;
 
-    const newMessage = { sender: "user", text: input };
+    const newMessage = { sender: "user", text: input, timestamp: new Date() };
     setMessages((prevMessages) => [...prevMessages, newMessage]);
     setInput("");
 
@@ -70,12 +79,17 @@ const ChatBox = ({
       conversation_id: conversationId,
       query: input,
     });
-    const bot_response = { sender: "bot", text: response.response };
+    const bot_response = {
+      sender: "bot",
+      text: response.response,
+      timestamp: new Date(),
+    };
     setLoading(false);
 
     setMessages((prevMessages) => [...prevMessages, bot_response]);
     setConversationId(response.conversation_id);
   };
+
   useEffect(() => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   }, [messages]);
@@ -85,65 +99,67 @@ const ChatBox = ({
   };
 
   return (
-    <div
-      className="flex flex-col h-full p-4 bg-gray-100"
-      style={{ minHeight: "calc(100vh - 16rem)" }}
-    >
+    <div className="flex flex-col" style={{ minHeight: "calc(100vh - 18rem)" }}>
       {isLoading || !chatEnabled ? (
-        <NoChatPlaceHolder />
+        <NoChatPlaceholder />
       ) : (
         <>
-          <div
-            className="flex-1 overflow-y-auto p-4 bg-white shadow-md rounded-lg"
-            style={{ maxHeight: "60vh" }}
-          >
+          <div className="flex-1 overflow-y-auto" style={{ maxHeight: "60vh" }}>
             {messages.map((message, index) => (
-              <div
+              <motion.div
                 key={index}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.3 }}
                 className={`mb-4 flex ${
                   message.sender === "user" ? "justify-end" : "justify-start"
                 }`}
               >
                 {message.sender === "bot" && (
-                  <LogoDark color="black" width="80" />
+                  <div
+                    className="flex-shrink-0 mr-2"
+                    style={{ minWidth: "40px", width: "40px" }}
+                  >
+                    <LogoDark color="black" width="100%" />
+                  </div>
                 )}
-
-                <div
-                  className={`p-3 rounded-lg max-w-xl ${
-                    message.sender === "user"
-                      ? "bg-primary text-white"
-                      : "bg-gray-300 text-black"
-                  }`}
-                >
+                <div className="p-3 rounded-lg max-w-xl bg-white text-black">
                   <ReactMarkdown
                     remarkPlugins={[remarkGfm]}
                     rehypePlugins={[rehypeRaw]}
                   >
                     {markify_text(message.text)}
                   </ReactMarkdown>
+                  <div className="text-xs text-gray-500 mt-2 text-right">
+                    {message.timestamp.toLocaleTimeString()}
+                  </div>
                 </div>
-              </div>
+              </motion.div>
             ))}
             {messages.length === 0 && (
               <div className="mb-4 flex justify-start">
-                <LogoDark color="black" width="80" />
-                <div className="p-3 rounded-lg max-w-xl bg-gray-300">
+                <div style={{ minWidth: "40px", width: "40px" }}>
+                  <LogoDark color="black" width="100%" />
+                </div>
+
+                <div className="p-3 rounded-lg max-w-xl bg-white text-black">
                   How can I help you today?
                 </div>
               </div>
             )}
             {loading && (
-              <div className="mb-4 flex justify-start">
-                <LogoDark color="black" width="80" />
-                <div>
-                  <ChatLoader />
+              <div className="my-4 flex justify-start">
+                <div style={{ minWidth: "40px", width: "40px" }}>
+                  <LogoDark color="black" width="100%" />
                 </div>
+
+                <ChatLoader />
               </div>
             )}
             <div ref={messagesEndRef} />
           </div>
-          <div className="mt-4 flex gap-4 items-top">
-            <div className="flex flex-grow">
+          <div className="mt-4 flex gap-4 items-center">
+            <div className="flex-grow relative">
               <Input
                 type="text"
                 value={input}
--- frontend/src/components/ChatLoader.tsx
+++ frontend/src/components/ChatLoader.tsx
@@ -3,9 +3,9 @@ import React from "react";
 
 const ChatLoader = () => {
   return (
-    <div >
-      <div id="bubble">
-        <div id="circleWrapper">
+    <div>
+      <div className="chat-bubble">
+        <div className="circle-wrapper">
           <div className="circle" id="circle1"></div>
           <div className="circle" id="circle2"></div>
           <div className="circle" id="circle3"></div>
