dc398a741d42910dc7332beba7bab77bc8bee498
ArslanSaleem <khan.arslan38@gmail.com>
2024-10-17 19:10:24 +0200

    fix: build issues

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -11,6 +11,13 @@ const nextConfig = {
       },
     ];
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.worker\.js$/,
+      use: { loader: "worker-loader" },
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
