3e89f657493706582cbeac68ed7ee1c51de8d15b
Arslan Saleem <khan.arslan38@gmail.com>
2024-10-25 16:00:47 +0200

    Fix[AI_field_Description]: error handling when credit limit reached  (#42)
    * fix[Field_description]: handle error in ai fields when credit limit reached
    
    * refactor[code]: improve error handling message and exception handling

--- frontend/src/services/extract.tsx
+++ frontend/src/services/extract.tsx
@@ -39,10 +39,18 @@ export const GetAIFieldDescriptions = async (
     );
     return response;
   } catch (error) {
-    if (axios.isAxiosError(error) && error.response?.data?.error) {
-      throw new Error(error.response.data.error);
+    if (axios.isAxiosError(error)) {
+      if (error.response?.data) {
+        throw new Error(error.response.data.detail);
+      } else {
+        throw new Error(
+          "Failed to generate AI field descriptions. Please try again."
+        );
+      }
     } else {
-      throw new Error("Failed to extract data. Please try again.");
+      throw new Error(
+        "Failed to generate AI field descriptions. Please try again."
+      );
     }
   }
 };
