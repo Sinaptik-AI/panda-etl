e8ecf9ae1a6bf2d64a236aa4a3e2fe2ade1ea4fd
Arslan Saleem <khan.arslan38@gmail.com>
2024-10-29 11:19:28 +0100

    feat(rollbar): add error boundaries on app and main view (#46)
    * feat(rollbar): add error boundaries on app and main view
    
    * refactor(rollbar): clean up error handling component
    
    * feat[Rollbar]: improve router logic

--- frontend/src/app/(app)/layout.tsx
+++ frontend/src/app/(app)/layout.tsx
@@ -4,6 +4,8 @@ import { ScrollProvider } from "@/context/ScrollContext";
 import Sidebar from "@/components/ui/Sidebar";
 import Navbar from "@/components/ui/Navbar";
 import "@/app/style/globals.css";
+import { ErrorBoundary } from "@rollbar/react";
+import { ViewErrorFallback } from "@/components/ErrorFallback";
 
 export default function RootLayout({
   children,
@@ -17,9 +19,11 @@ export default function RootLayout({
           <Sidebar />
           <div className="flex-1 flex flex-col overflow-hidden md:ml-64">
             <Navbar />
-            <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-50 px-10 py-6">
-              {children}
-            </main>
+            <ErrorBoundary fallbackUI={ViewErrorFallback}>
+              <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-50 px-10 py-6">
+                {children}
+              </main>
+            </ErrorBoundary>
           </div>
         </div>
       </ScrollProvider>
